06a57c9cbb431259f7b04e865cc24a92266348fe
diegoorbe96 <63675814+diegoorbe96@users.noreply.github.com>
2026-01-30 00:06:20 -0300

    No se asusten
    - Arme un NPC-base para que todos compartan comportamiento.
    -- Cada NPC tiene una textura y un dialogo como export.
    
    - El NPC tiene una StateMachine (muy chota) para el movimiento

---
+++ ggj-26---carbono/GlobalData.gd
@@ -0,0 +1,6 @@
+extends Node
+signal mask_changed
+
+
+var is_lying_mask_equipped: bool = false
+var is_in_dialogue: bool = false
--- ggj-26---carbono/player.gd
+++ ggj-26---carbono/player.gd
@@ -2,7 +2,20 @@ extends CharacterBody2D
 
 var speed = 300.0
 
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if not GlobalData.is_in_dialogue:
+		if event.is_action_pressed("change_mask") and not GlobalData.is_lying_mask_equipped:
+			GlobalData.is_lying_mask_equipped = true
+			$Sprite2D.modulate = Color.RED
+		elif event.is_action_pressed("change_mask") and GlobalData.is_lying_mask_equipped:
+			GlobalData.is_lying_mask_equipped = false
+			$Sprite2D.modulate = Color.WHITE
+
+
 func _physics_process(delta: float) -> void:
-	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	velocity = direction*speed
-	move_and_slide()
+	if not GlobalData.is_in_dialogue:
+		var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+		velocity = direction*speed
+		move_and_slide()

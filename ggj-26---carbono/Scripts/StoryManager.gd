945a36444cf48f70512044aa8d2d7b283d5e0fb2
diegoorbe96 <63675814+diegoorbe96@users.noreply.github.com>
2026-01-31 15:17:16 -0300

    Dialogo con Lord de Mansion Ready

--- ggj-26---carbono/Scripts/StoryManager.gd
+++ ggj-26---carbono/Scripts/StoryManager.gd
@@ -10,7 +10,14 @@ var story_database = {
 	"lord_trusts_you": "El lord de la mansión confia en mi.",
 	"thermo_obtained": "El chef me dio un termometro para carne.",
 	"need_lord_trust": "El chef solo me dará el termometro si obtengo la confianza del dueño de la mansión.",
-	"chef_not_know_pianist": "El chef nunca hablo con la pianista."
+	"chef_not_know_pianist": "El chef nunca hablo con la pianista.",
+	"lord_claims_accident": "El dueño de la mansión dice que la muerte fue un accidente.",
+	"cadaver_checked": "He inspeccionado el cadaver, definitivamente fue un asesinato.",
+	"daughter_contracted_pianist": "El dueño de la mansion dijo que fue su hija quien contrato a la pianista.",
+	"lord_arrived_late": "El dueño de la mansion llego a la escena despues que la pianista.",
+	"lord_and_friends_cant_see": "El dueño de la mansion y sus amigos ya estan grandes... No ven bien y sufren varios dolores.",
+	"lord_may_be_aggresive": "El dueño no quiere hablar de su forma de 'disciplina'.. Acaso su hija sera victima de violencia familiar? ¿Habra intentado escapar?"
+	
 }
 var unlocked_beats: Array = []
 

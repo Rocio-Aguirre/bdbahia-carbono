6de5e3c737799c3fac72ad008d18c6df0b27814e
diegoorbe96 <63675814+diegoorbe96@users.noreply.github.com>
2026-01-31 23:46:50 -0300

    Todos los dialogos ready

--- ggj-26---carbono/Scripts/StoryManager.gd
+++ ggj-26---carbono/Scripts/StoryManager.gd
@@ -6,18 +6,37 @@ var story_database = {
 	"masks_acknowledged": "Se ve que existen mascaras que obligan a decir la verdad, y mascaras que obligan a mentir... Interesante.",
 	"chef_hates_daughter": "El chef dice que la hija del dueño era insoportable.",
 	"lord_honorable": "El chef dice que el dueño de la mansión es un hombre honorable, y que es su amigo.",
-	"lord_strict": "Pareciera que el dueño era muy estricto con su hija.",
-	"lord_trusts_you": "El lord de la mansión confia en mi.",
+	"chef_outage_story": "El chef dice que se corto la luz, y que siguio sonando la musica.",
+	"chef_found_victim": "El chef dice que luego del corte de luz, escucho un estruendo y encontro el cadaver en la sala.",
+	"chef_claims_lord_strict": "El chef dice que el dueño era estricto con la hija, aunque no suficiente.",
+	"chef_likes_today_music": "El chef dice que el desempeño de la pianista fue pobre durante la semana, pero que extrañamente hoy mejoro mucho.",
+	"chef_claims_cooking": "El chef dice que se ensucio las manos cocinando",
 	"thermo_obtained": "El chef me dio un termometro para carne.",
-	"need_lord_trust": "El chef solo me dará el termometro si obtengo la confianza del dueño de la mansión.",
 	"chef_not_know_pianist": "El chef nunca hablo con la pianista.",
-	"lord_claims_accident": "El dueño de la mansión dice que la muerte fue un accidente.",
-	"cadaver_checked": "He inspeccionado el cadaver, definitivamente fue un asesinato.",
-	"daughter_contracted_pianist": "El dueño de la mansion dijo que fue su hija quien contrato a la pianista.",
+	"need_lord_trust": "El chef solo me dará el termometro si obtengo la confianza del dueño de la mansión.",
+
+	"lord_strict": "Pareciera que el dueño era muy estricto con su hija. Segun el, su hija era muy irrespetuosa.",
+	"lord_trusts_you": "El dueño de la mansión confia en mi.",
+	"lord_claims_accident": "El dueño dice que la muerte fue un accidente.",
+	"daughter_contracted_pianist": "El dueño de la mansion dijo que fue su hija quien contrato a la pianista hace unas semana.",
+	"daughter_overprotective_piano": "El dueño dijo que su hija es muy celosa de quien toca su piano",
 	"lord_arrived_late": "El dueño de la mansion llego a la escena despues que la pianista.",
-	"lord_and_friends_cant_see": "El dueño de la mansion y sus amigos ya estan grandes... No ven bien y sufren varios dolores.",
-	"lord_may_be_aggresive": "El dueño no quiere hablar de su forma de 'disciplina'.. Acaso su hija sera victima de violencia familiar? ¿Habra intentado escapar?"
+	"lord_claims_cant_see": "El dueño de la mansion dice que no puede ver bien.",
+	"lord_mixed_up_daughter": "El dueño se confundio a la pianista con su hija.",
+	"lord_may_be_aggresive": "El dueño no quiere hablar de su forma de 'disciplina'.. Esperemos que no sea violento",
+	"lord_mixed_up_guard": "El dueño me confundio con el guardia, a pesar de que no somos parecidos.",
 	
+	"pianist_contrated_by_lord": "La pianista dijo que fue contratada por el dueño hace unos meses.",
+	"pianist_likes_lord": "La pianista dijo que el dueño nunca fue violento con ella.",
+	"pianist_likes_daughter": "La pianista dijo que la hija del dueño era una persona respetable.",
+	"pianist_cant_play_dark": "La pianista dijo que le seria imposible tocar el piano en la oscuridad",
+	"pianist_hurt": "La pianista tiene moretones en el cuello. Ella afirma que fueron un accidente con las cuerdas del piano",
+	"pianist_overprotective_piano": "A pesar de parecer una persona calma, cuando intente tocar el piano, la pianista casi me ataca.",
+	
+	"cadaver_checked": "He inspeccionado el cadaver, definitivamente fue un asesinato.",
+	"corpse_hands_inspected": "Las manos del cuerpo estan llenas de callos, parecen de alguien que trabajo toda su vida en el campo",
+	"corpse_glued_mask": "Extrañamente, la mascara se encuentra pegada al cadaver... ¿Por que harian esto?",
+	"pianist_can_play_dark": "Cuando apague la luz, la pianista siguio tocando musica."
 }
 var unlocked_beats: Array = []
 
